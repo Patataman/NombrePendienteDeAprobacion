f80d454813d34dc0917f0e5a342519a644189425
Patataman <shas_arroyo@hotmail.com>
2016-08-15 18:12:29 +0200

    juntar cosicas

--- classes/Scene.py
+++ classes/Scene.py
@@ -136,4 +136,54 @@ class ScenePanel(Scene):
         screen.fill((0,0,0))
 
 class SceneFight(Scene):
-    pass
+
+    def __init__(self, director):
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("FightSene")
+        background_image = load_image('assets/images/fondoX.png')
+        # Se inicializan los personajes y avatares
+        player1 = Player("pepi")
+        player2 = Player("pepo")
+        avatar1 = load_image("assets/images/pepi_avatarI.png")
+        avatar1Rect = avatar1.get_rect()
+        avatar1Rect.centerx = 41
+        avatar1Rect.centery = 40
+        avatar2 = load_image("assets/images/pepo_avatarD.png")
+        avatar2Rect = avatar2.get_rect()
+        avatar2Rect.centerx = 983
+        avatar2Rect.centery = 40
+        
+        clock = pygame.time.Clock()
+        couldown = 60 # tiempo de combate
+        time2 = 0
+
+    # Barras de vida
+    hudP1 = pygame.Rect(60, 40, 400, 10)
+    hudP2 = pygame.Rect(564, 40, 400, 10)
+
+    def on_update(self, time):
+        hudP1.width = player1.health*4
+        hudP2.width = player2.health*4
+        hudP2.left = 564+(400-player2.health*4)
+        time2 += time
+
+    def on_event(self, time, event):
+        if keys[K_a]:
+            player1.getHurt(5)
+        elif keys[K_d]:
+            player1.getHurt(12)
+        elif keys[K_j]:
+            player2.getHurt(5)
+        elif keys[K_l]:
+            player2.getHurt(12)
+
+    def on_draw(self, screen):
+        screen.blit(background_image, (0, 0))
+        timeCD, time_rect = texto(str(couldown - int(math.floor(time2/1000))), 512, 45)
+
+        screen.blit(timeCD, time_rect)
+        pygame.draw.rect(screen,(255,255,255),hudP1)
+        pygame.draw.rect(screen,(255,255,255),hudP2)
+        screen.blit(avatar1, avatar1Rect)
+        screen.blit(avatar2, avatar2Rect)
+        pygame.display.flip()
